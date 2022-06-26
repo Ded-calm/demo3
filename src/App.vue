eb60cdd4493e2ef4cd9a146b8d2c02c9ec36fde3
leizi <75164144+lei-zi666@users.noreply.github.com>
2022-06-26 16:49:59 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,36 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header></Header>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+// 导入头部组件
+// D:\VueCode\vueDemo03\demo3\src\components\Header.vue
+import Header from "@/components/Header.vue";
 export default {
-  name: 'App',
+  name: "App",
+
+  data() {
+    return {};
+  },
+
+  // 生命周期函数
+  mounted() {},
+
+  methods: {},
+
   components: {
-    HelloWorld
-  }
-}
+    Header,
+  },
+};
 </script>
 
-<style>
+<style scoped>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 60%;
+  /* 居中 */
+  margin: 0 auto;
+  background-color: #f2f2f2;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id="box">
+        <h1>购物车</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+
+    data() {
+        return {
+            
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+
+<style scoped>
+
+</style>
